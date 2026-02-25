df280ae1d3c8d878b786538e1ca959c9923aaed0
qhauuy <qhauuy@student.42mulhouse.fr>
2026-02-25 09:44:34 +0100

    blabla

--- repo/src/main.js
+++ repo/src/main.js
@@ -1,1 +1,10 @@
-document.querySelector("#app").textContent = "test";
+import "./style.css";
+import s from "./main.module.css";
+
+document.querySelector("#app").innerHTML = `
+  <div class="${s.root}">
+    <h1 class="${s.title}">test</h1>
+    <div class="${s.section}">blabla</div>
+    <div class="${s.section}">blabla</div>
+  </div>
+`;
